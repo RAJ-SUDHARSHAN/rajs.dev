3bacf460121e4dd79ed3cf2c9082fd37f11af767
Raj Sudharshan <sudharshann05@gmail.com>
2023-05-26 00:39:50 -0700

    update variables for cicd buddy pipeline

--- raj_portfolio/settings.py
+++ raj_portfolio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c)a_g=_u8)9&^f0(0t4o)jzy02p6*eranrw!%+1ebx=t@yw2#z'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
-ALLOWED_HOSTS = ['rajs.dev']
 
 
 # Application definition
